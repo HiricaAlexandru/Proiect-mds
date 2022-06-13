8cf4a08b5bacfde4fb4d15e12a39776f14a14eb9
Tăbăcaru Andrei <47+andrei-tabacaru@users.noreply.github.com>
2022-06-13 22:43:02 +0300

    Updated game.py
    Finished DrawConsoleBoard, update, _init and reset functions.

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import board
 import main
+import pygame
 
 class Game:
     #Clasa care se ocupa de gestionarea jocului
@@ -12,16 +13,41 @@ class Game:
         self.noMoves = 0
 
     def DrawConsoleBoard(self):
-        pass
+        self.noMoves += 1
+        # Afisez in consola configuratia curenta a tablei de joc si cine muta
+        if self.turn == self.board.computer_color:
+            print("Computer moves")
+        else:
+            print("Player moves")
+
+        for r in range(main.ROWS):
+            for c in range(main.COLUMNS):
+                if self.board.board[r][c] == 0:
+                    print('#', end=" ")
+                elif self.board.board[r][c].color == main.BLACK:
+                    print('B', end=" ")
+                else:
+                    print("W", end=" ")
+            print()
+        print('\n')
 
     def update(self):
-        pass
+        # actualizez jocul in interfata grafica
+        self.board.draw(self.window)
+        self.draw_valid_moves(self.valid_moves)
+        pygame.display.update()
 
     def _init(self, color):
-        pass
+        # initializez iar jocul
+        self.selected = None
+        self.board = board.Board(color)
+        self.turn = main.BLACK
+        self.valid_moves = {}
 
     def reset(self, color):
-        pass
+        # resetez jocul
+        self.DrawConsoleBoard()
+        self._init(color)
 
     def select(self, row, col):
         pass
