78c3d5f15bba93d2e1be3c614883a667d109ef3c
HiricaAlexandru <72924378+HiricaAlexandru@users.noreply.github.com>
2022-06-14 17:11:11 +0300

    added alpha beta algorithm

--- algorithms.py
+++ algorithms.py
@@ -69,3 +69,48 @@ def MiniMax(board, depth, JMAX, game, noOfNodes):
             if minEval == evaluation:
                 bestMove = move
         return minEval, bestMove
+
+def AlphaBeta(alpha, beta, board, depth, JMAX, game, noOfNodes):
+    """
+    :param alpha: valoarea lui alpha
+    :param beta: valoarea lui beta
+    :param board: configuratia actuala
+    :param depth: adancimea pana a care vreau sa merg
+    :param JMAX: verific daca este jucatorul care incearca sa maximizeze
+    :param game: jocul curent
+    :return: Evaluarea mutarii si tabla
+    Primeste tabla cu toate piese la momentul curent, cate mutari vreau sa consider, daca vreau sa maximizez scorul sau sa il minimizez
+    """
+    if depth == 0 or board.winner() != None:        #daca am ajuns la adancimea maxima sau daca am gasit un castigator
+        return board.evaluateScore1(), board      #evaluez configuratia curenta si o returnez
+    if alpha > beta:
+        return board.evaluateScore1(), board
+    if JMAX:    #evaluez pentru jucatorul care incearca sa maximizeze scorul
+        maxEval = float('-inf')
+        bestMove = None
+        for move in GetAllMoves(board, game.getBoard().computer_color, game):        #parcurg toate mutarile posibile pentru jucatorul JMAX
+            noOfNodes += 1
+            evaluation = MiniMax(move, depth - 1, False, game, noOfNodes)[0]
+            maxEval = max(maxEval, evaluation)
+            if alpha < evaluation:
+                alpha = evaluation
+                if alpha >= beta:
+                    break
+            if maxEval == evaluation:
+                bestMove = move
+        return maxEval, bestMove, noOfNodes
+
+    else:       #evaluez pentru jucatorul care incearca sa minimizeze scorul
+        minEval = float('inf')
+        bestMove = None
+        for move in GetAllMoves(board, game.getBoard().user_color, game):        #parcurg toate mutarile posibile pentru jucatorul JMIN
+            noOfNodes += 1
+            evaluation = MiniMax(move, depth - 1, True, game, noOfNodes)[0]
+            minEval = min(minEval, evaluation)
+            if beta > evaluation:
+                beta = evaluation
+                if alpha >= beta:
+                    break
+            if minEval == evaluation:
+                bestMove = move
+        return minEval, bestMove, noOfNodes
