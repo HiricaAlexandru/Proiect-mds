47c0f8c583e6a8f9d03e8fb5facac8f6da784810
SofiVultur <92263496+SofiVultur@users.noreply.github.com>
2022-06-14 12:00:32 +0300

    Created GrupButoane class for displaying and managing main menu buttons

---
+++ grupButoane.py
@@ -0,0 +1,34 @@
+import button
+import main
+
+class GrupButoane:
+    def __init__(
+            self, listaButoane=[], indiceSelectat=0, spatiuButoane=10, left=0, top=0
+    ):
+        self.listaButoane = listaButoane
+        self.indiceSelectat = indiceSelectat
+        self.listaButoane[self.indiceSelectat].selectat = True
+        self.top = top
+        self.left = left
+        leftCurent = self.left
+        for b in self.listaButoane:
+            b.top = self.top
+            b.left = leftCurent
+            b.updateDreptunghi()
+            leftCurent += spatiuButoane + b.w
+
+    def selecteazaDupacoord(self, coord):
+        for ib, b in enumerate(self.listaButoane):
+            if b.selecteazaDupacoord(coord):
+                self.listaButoane[self.indiceSelectat].selecteaza(False)
+                self.indiceSelectat = ib
+                return True
+        return False
+
+    def deseneaza(self):
+        # atentie, nu face wrap
+        for b in self.listaButoane:
+            b.deseneaza()
+
+    def getValoare(self):
+        return self.listaButoane[self.indiceSelectat].valoare
