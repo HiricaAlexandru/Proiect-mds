bccb8fb04b0465b0b732622bf5064eb0a3f3b2eb
SofiVultur <92263496+SofiVultur@users.noreply.github.com>
2022-06-15 21:09:14 +0300

    Refactored main and created Singleton game manager class

--- main.py
+++ main.py
@@ -135,83 +135,138 @@ def get_with_mouse(pos):
 
     return row, col
 
+class SesiuneJoc():
+    __instance__=None
 
-def main():
-    pygame.init()
-    run = True
-    game = gme.Game(WINDOW, "CREAM")
-    color, algorithm, depth, game_type = deseneaza_alegeri(WINDOW, game)
-    pygame.mixer.music.load('muzica_populara.wav')
-    pygame.mixer.music.play(-1)
+    def __init__(self):
 
-    
-    if color == "BLACK":
-        user_color = BLACK
-        computer_color = RED
-    else:
-        user_color = RED
-        computer_color = BLACK
-    #am jocul computer vs player
-    if game_type == "cp":
-        initial_time_game = int(round(time.time() * 1000))
-        #initializez pentru a putea afisa la final statisticile
-        min_nodes = float('inf')
-        max_nodes = float('-inf')
-        media_nodes = 0
-        min_time = float('inf')
-        max_time = float('-inf')
-        media_time = 0
-        nr_runde = 0
+        if SesiuneJoc.__instance__ is None:
+            SesiuneJoc.__instance__ = self
+        else:
+            raise Exception("O sesiune poate fi accesata doar de catre un utilizator!")
 
-        while run:
-            #Verific daca mai am mutari posibile pentru culoarea curenta
-            #Daca culoarea curenta s-a blocat atunci opresc jocul
-            moreMoves = len(alg.GetAllMoves(game.board, game.turn, game))
-            if moreMoves == 0:
-                if game.turn == computer_color:
-                    print("Computerul nu mai are mutari. A castigat playerul")
-                else:
-                    print("Playerul nu mai are mutari. A castigat computerul")
-                run = False
+    @staticmethod
+    def get_instance():
+        # We define the static method to fetch instance
+        if not SesiuneJoc.__instance__:
+            SesiuneJoc()
+        return SesiuneJoc.__instance__
 
-            if game.turn == computer_color and moreMoves != 0:
-                t_inainte = int(round(time.time() * 1000))
-                if algorithm == "alphabeta":
-                    value, new_board, nodes = alg.AlphaBeta(float("-inf"), float("inf"), game.getBoard(), int(depth), computer_color, game, 0)
-                    game.computerMove(new_board)
-                else:
-                    value, new_board, nodes = alg.MiniMax(game.getBoard(), int(depth), computer_color, game, 0)
-                    game.computerMove(new_board)
 
-                t_dupa = int(round(time.time() * 1000))
-                print(
-                    'Calculatorul a "gandit" timp de '
-                    + str(t_dupa - t_inainte)
-                    + " milisecunde."
-                )
-                nr_runde += 1
+    def start_joc(self):
+        pygame.init()
+        run = True
+        game = gme.Game(WINDOW, "CREAM")
+        color, algorithm, depth, game_type = deseneaza_alegeri(WINDOW, game)
+        pygame.mixer.music.load('muzica_populara.wav')
+        pygame.mixer.music.play(-1)
 
-                print("Numarul de noduri: ", nodes)
-                if nodes < min_nodes:
-                    min_nodes = nodes
-                elif nodes > max_nodes:
-                    max_nodes = nodes
-                media_nodes += nodes
 
-                media_time += (t_dupa - t_inainte)
-                if t_dupa - t_inainte > max_time:
-                    max_time = t_dupa - t_inainte
-                elif t_dupa - t_inainte < min_time:
-                    min_time = t_dupa - t_inainte
+        if color == "BLACK":
+            user_color = BLACK
+            computer_color = RED
+        else:
+            user_color = RED
+            computer_color = BLACK
+        #am jocul computer vs player
+        if game_type == "cp":
+            initial_time_game = int(round(time.time() * 1000))
+            #initializez pentru a putea afisa la final statisticile
+            min_nodes = float('inf')
+            max_nodes = float('-inf')
+            media_nodes = 0
+            min_time = float('inf')
+            max_time = float('-inf')
+            media_time = 0
+            nr_runde = 0
 
-            if game.winner() != None:
-                if game.winner() == game.getBoard().computer_color:
-                    print("Computer won")
-                else: print("Player won")
-                run = False
+            while run:
+                #Verific daca mai am mutari posibile pentru culoarea curenta
+                #Daca culoarea curenta s-a blocat atunci opresc jocul
+                moreMoves = len(alg.GetAllMoves(game.board, game.turn, game))
+                if moreMoves == 0:
+                    if game.turn == computer_color:
+                        print("Computerul nu mai are mutari. A castigat playerul")
+                    else:
+                        print("Playerul nu mai are mutari. A castigat computerul")
+                    run = False
 
-            if game.turn == user_color and moreMoves != 0:
-                t_inainte = int(round(time.time() * 1000))
+                if game.turn == computer_color and moreMoves != 0:
+                    t_inainte = int(round(time.time() * 1000))
+                    if algorithm == "alphabeta":
+                        value, new_board, nodes = alg.AlphaBeta(float("-inf"), float("inf"), game.getBoard(), int(depth), computer_color, game, 0)
+                        game.computerMove(new_board)
+                    else:
+                        value, new_board, nodes = alg.MiniMax(game.getBoard(), int(depth), computer_color, game, 0)
+                        game.computerMove(new_board)
+
+                    t_dupa = int(round(time.time() * 1000))
+                    print(
+                        'Calculatorul a "gandit" timp de '
+                        + str(t_dupa - t_inainte)
+                        + " milisecunde."
+                    )
+                    nr_runde += 1
+
+                    print("Numarul de noduri: ", nodes)
+                    if nodes < min_nodes:
+                        min_nodes = nodes
+                    elif nodes > max_nodes:
+                        max_nodes = nodes
+                    media_nodes += nodes
+
+                    media_time += (t_dupa - t_inainte)
+                    if t_dupa - t_inainte > max_time:
+                        max_time = t_dupa - t_inainte
+                    elif t_dupa - t_inainte < min_time:
+                        min_time = t_dupa - t_inainte
+
+                if game.winner() != None:
+                    if game.winner() == game.getBoard().computer_color:
+                        print("Computer won")
+                    else: print("Player won")
+                    run = False
+
+                if game.turn == user_color and moreMoves != 0:
+                    t_inainte = int(round(time.time() * 1000))
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            run = False
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            pos = pygame.mouse.get_pos()
+                            row, col = get_with_mouse(pos)
+                            game.select(row, col)
+                            t_dupa = int(round(time.time() * 1000))
+                            if t_dupa - t_inainte != 0:
+                                print(
+                                    'Player-ul a "gandit" timp de '
+                                    + str(t_dupa - t_inainte)
+                                    + " milisecunde."
+                                )
+
+                game.update()
+            print("\nStatistici pentru calculator")
+            print("Timpul minim al calculatorului: " + str(min_time) + " milisecunde" )
+            print("Timpul maxim al calculatorului: " +  str(max_time) + " milisecunde")
+            print("Media de timp: " + str(media_time / nr_runde) + " milisecunde" )
+            print("Numarul minim de noduri: ", min_nodes)
+            print("Numarul maxim de noduri: ", max_nodes)
+            print("Media de noduri: ", media_nodes / nr_runde)
+            final_time_game = int(round(time.time() * 1000))
+            print("\nTimpul total de joc:", final_time_game - initial_time_game, "milisecunde")
+            print("Numar total de mutari: ", game.noMoves)
+            pygame.quit()
+        #am jocul player vs player
+        elif game_type == "pp":
+            while run:
+
+                if game.winner() != None:
+                    if game.winner() == game.getBoard().computer_color:
+                        print("Computer won")
+                    else:
+                        print("Player won")
+                    run = False
+
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         run = False
@@ -219,76 +274,39 @@ def main():
                         pos = pygame.mouse.get_pos()
                         row, col = get_with_mouse(pos)
                         game.select(row, col)
-                        t_dupa = int(round(time.time() * 1000))
-                        if t_dupa - t_inainte != 0:
-                            print(
-                                'Player-ul a "gandit" timp de '
-                                + str(t_dupa - t_inainte)
-                                + " milisecunde."
-                            )
-
-            game.update()
-        print("\nStatistici pentru calculator")
-        print("Timpul minim al calculatorului: " + str(min_time) + " milisecunde" )
-        print("Timpul maxim al calculatorului: " +  str(max_time) + " milisecunde")
-        print("Media de timp: " + str(media_time / nr_runde) + " milisecunde" )
-        print("Numarul minim de noduri: ", min_nodes)
-        print("Numarul maxim de noduri: ", max_nodes)
-        print("Media de noduri: ", media_nodes / nr_runde)
-        final_time_game = int(round(time.time() * 1000))
-        print("\nTimpul total de joc:", final_time_game - initial_time_game, "milisecunde")
-        print("Numar total de mutari: ", game.noMoves)
-        pygame.quit()
-    #am jocul player vs player
-    elif game_type == "pp":
-        while run:
-
-            if game.winner() != None:
-                if game.winner() == game.getBoard().computer_color:
-                    print("Computer won")
-                else:
-                    print("Player won")
-                run = False
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                game.update()
+            pygame.quit()
+        #am jocul computer vs computer
+        else:
+            noOfNodes = 0
+            while run:
+                time.sleep(1)
+                if game.winner() != None:
+                    if game.winner() == game.getBoard().computer_color:
+                        print("Computer won")
+                    else:
+                        print("Player won")
                     run = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    pos = pygame.mouse.get_pos()
-                    row, col = get_with_mouse(pos)
-                    game.select(row, col)
-            game.update()
-        pygame.quit()
-    #am jocul computer vs computer
-    else:
-        noOfNodes = 0
-        while run:
-            time.sleep(1)
-            if game.winner() != None:
-                if game.winner() == game.getBoard().computer_color:
-                    print("Computer won")
-                else:
-                    print("Player won")
-                run = False
 
-            if game.turn == computer_color:
-                if algorithm == "alphabeta":
-                    value, new_board, noOfNodes = alg.AlphaBeta(-500, 500, game.getBoard(), int(depth), computer_color, game, noOfNodes)
-                    game.computerMove(new_board)
+                if game.turn == computer_color:
+                    if algorithm == "alphabeta":
+                        value, new_board, noOfNodes = alg.AlphaBeta(-500, 500, game.getBoard(), int(depth), computer_color, game, noOfNodes)
+                        game.computerMove(new_board)
+                    else:
+                        value, new_board, noOfNodes = alg.MiniMax(game.getBoard(), int(depth), computer_color, game, noOfNodes)
+                        game.computerMove(new_board)
                 else:
-                    value, new_board, noOfNodes = alg.MiniMax(game.getBoard(), int(depth), computer_color, game, noOfNodes)
-                    game.computerMove(new_board)
-            else:
-                if algorithm == "alphabeta":
-                    value, new_board, noOfNodes = alg.AlphaBeta(-500, 500, game.getBoard(), int(depth), user_color, game, noOfNodes)
-                    game.computerMove(new_board)
-                else:
-                    value, new_board, noOfNodes = alg.MiniMax(game.getBoard(), int(depth), user_color, game, noOfNodes)
-                    game.computerMove(new_board)
+                    if algorithm == "alphabeta":
+                        value, new_board, noOfNodes = alg.AlphaBeta(-500, 500, game.getBoard(), int(depth), user_color, game, noOfNodes)
+                        game.computerMove(new_board)
+                    else:
+                        value, new_board, noOfNodes = alg.MiniMax(game.getBoard(), int(depth), user_color, game, noOfNodes)
+                        game.computerMove(new_board)
 
-            game.update()
-        
-        pygame.quit()
+                game.update()
 
-main()
+            pygame.quit()
+
+joc=SesiuneJoc()
+joc.start_joc()
 
